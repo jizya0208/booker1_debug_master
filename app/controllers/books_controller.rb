bc3e4b3baec4bf4eaa56968a71818a4508e7a09f
EC2 Default User <ec2-user@ip-172-31-55-167.ec2.internal>
2021-06-01 04:34:10 +0000

    [FIX] インスタンス変数記述位置修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,11 +9,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @books = Book.all
     if @book.save
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      #　記述位置間違え。模範回答をもとに修正。
+      @books = Book.all
       render :index
     end
   end
